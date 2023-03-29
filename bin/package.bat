14364e88ab0a1d6f66c651f33ed930d1e2b24089
zZhenMing <zhangzm@fortunetech.cn>
2023-03-29 15:13:52 +0800

    zx提交

--- bin/package.bat
+++ bin/package.bat
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 @echo off
 echo.
 echo [信息] 打包Web工程，生成war/jar包文件。
@@ -9,4 +10,17 @@ cd %~dp0
 cd ..
 call mvn clean package -Dmaven.test.skip=true
 
+=======
+@echo off
+echo.
+echo [信息] 安装Web工程，生成node_modules文件。
+echo.
+
+%~d0
+cd %~dp0
+
+cd ..
+npm install --registry=https://registry.npmmirror.com
+
+>>>>>>> 12cfc7c (zx鎻愪氦)
 pause
---
+++ src/views/index.vue
@@ -0,0 +1,99 @@
+<template>
+  <div style="text-align: center; height: 100%; width: ;">
+    <!-- <span style="font-size: 300px; vertical-align: middle;color: purple;">潘总牛逼</span> -->
+    <echartCompent />
+  </div>
+</template>
+
+<script>
+import echartCompent from '../components/echarts/index';
+
+export default {
+  name: "Index",
+  components: {
+    echartCompent,
+  },
+  data() {
+    return {
+      // 版本号
+      version: "3.8.5"
+    };
+  },
+  methods: {
+    goTarget(href) {
+      window.open(href, "_blank");
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.home {
+  blockquote {
+    padding: 10px 20px;
+    margin: 0 0 20px;
+    font-size: 17.5px;
+    border-left: 5px solid #eee;
+  }
+
+  hr {
+    margin-top: 20px;
+    margin-bottom: 20px;
+    border: 0;
+    border-top: 1px solid #eee;
+  }
+
+  .col-item {
+    margin-bottom: 20px;
+  }
+
+  ul {
+    padding: 0;
+    margin: 0;
+  }
+
+  font-family: "open sans",
+  "Helvetica Neue",
+  Helvetica,
+  Arial,
+  sans-serif;
+  font-size: 13px;
+  color: #676a6c;
+  overflow-x: hidden;
+
+  ul {
+    list-style-type: none;
+  }
+
+  h4 {
+    margin-top: 0px;
+  }
+
+  h2 {
+    margin-top: 10px;
+    font-size: 26px;
+    font-weight: 100;
+  }
+
+  p {
+    margin-top: 10px;
+
+    b {
+      font-weight: 700;
+    }
+  }
+
+  .update-log {
+    ol {
+      display: block;
+      list-style-type: decimal;
+      margin-block-start: 1em;
+      margin-block-end: 1em;
+      margin-inline-start: 0;
+      margin-inline-end: 0;
+      padding-inline-start: 40px;
+    }
+  }
+}
+</style>
+
